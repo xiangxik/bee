e608b838402d37cc95ae5a1b5a98481c324cda7e
Ken Kong <156801554@qq.com>
2020-09-21 22:47:07 +0800

    fix

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   version: "2.9.0",
   note: '增加拼团功能',
-  subDomain: "beeorder", // 根据教程 https://www.it120.cc/help/qr6l4m.html 查看并设置你自己的 subDomain
+  subDomain: "ken", // 根据教程 https://www.it120.cc/help/qr6l4m.html 查看并设置你自己的 subDomain
 }
